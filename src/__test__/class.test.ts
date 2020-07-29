d86437767f1b5163f548614a9c910e6b8ee92977
Blayne Chard <bchard@linz.govt.nz>
2020-07-29 13:55:19 +1200

    build: adding formatting checks

---
+++ src/__test__/class.test.ts
@@ -0,0 +1,19 @@
+export class FooBar {
+  get foo(): number {
+    return 1;
+  }
+
+  async bar(): Promise<string> {
+    return 'bar';
+  }
+
+  /**
+   * @param foo foo to bar
+   */
+  fooBar(foo = 'foo'): string {
+    return `${foo}bar`;
+  }
+}
+
+export const Foo = new FooBar();
+export const FooMaker = (f: string): string => new FooBar().fooBar(f);
